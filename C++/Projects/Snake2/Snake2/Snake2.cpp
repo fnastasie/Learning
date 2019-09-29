c3952614eda6423ed8d684338b1c5217cbd855f0
Filip Nastasie <46202792+fnastasie@users.noreply.github.com>
2019-09-29 17:44:19 +0100

    .

--- C++/Projects/Snake2/Snake2/Snake2.cpp
+++ C++/Projects/Snake2/Snake2/Snake2.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <Windows.h>
 using namespace std;
+
 bool gameOver;
 const int width = 20;
 const int height = 20;
@@ -20,49 +21,7 @@ void Setup()
 	fruitY = rand() % height;
 	score = 0;
 }
-void Draw()
-{
-	system("cls");
-	for (int i = 0; i < width + 2; i++)
-		cout << "#";
-	cout << endl;
 
-	for (int i = 0; i < width; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			if (j == 0)
-				cout << "#";
-			if (i == y && j == x)
-				cout << "O";
-			else if (i == fruitY && j == fruitX)
-				cout << "F";
-			else
-			{
-				bool print = false;
-				for (int k = 0; k < nTail; k++)
-				{
-					if (tailX[k] == j && tailY[k] == i)
-					{
-						cout << "o";
-						print = true;
-					}
-				}
-				if (!print)
-					cout << " ";
-			}
-
-			if (j == width - 1)
-				cout << "#";
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < width + 2; i++)
-		cout << "#";
-	cout << endl;
-	cout << "Score:" << score << endl;
-}
 void Input()
 {
 	if (_kbhit())
